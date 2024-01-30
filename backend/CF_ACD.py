f538fc320c14bd315f14a3045c4ad0d529ed0879
Pal_314 <4130.stkabirdin@gmail.com>
2024-01-30 23:38:05 +0530

    Refactor API endpoints and add new functionality

--- backend/CF_ACD.py
+++ backend/CF_ACD.py
@@ -7,51 +7,21 @@ app = FastAPI()
 app.add_middleware(cors.CORSMiddleware, allow_origins=["*"])
 
 
-@app.get("/")
-def home():
-    return {"Hello": "World"}
-
-
-@app.get("/ladder")
-async def ladder(lowerlimit: str, upperlimit: str):
-    if int(lowerlimit) >= int(upperlimit):
-        raise RequestValidationError(
-            [
-                {
-                    "loc": ("lowerlimit", "upperlimit"),
-                    "msg": "lowerlimit should be less than upperlimit",
-                }
-            ]
-        )
+@app.get("/get_rating/{handle}")
+async def get_rating(handle: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            "https://acodedaily.com/api/v2/ladder?startRating="
-            + lowerlimit
-            + "&endRating="
-            + upperlimit
-        ) as response:
-            if response.status != 200:
-                return HTTPException(
-                    status_code=response.status, detail="ACD API is down"
-                )
-            data = await response.json()
-            return list(item["name"] for item in data["data"])
-
-
-@app.get("/submissions/{handle}")
-async def submissions(handle: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            "https://codeforces.com/api/user.status?handle=" + handle
-        ) as response:
-            data = await response.json()
-            return list(
-                set(
-                    item["problem"]["name"]
-                    for item in data["result"]
-                    if item["verdict"] == "OK"
-                )
-            )
+        try:
+            async with session.get(
+                "https://codeforces.com/api/user.info?handles=" + handle
+            ) as response:
+                if response.status != 200:
+                    return HTTPException(
+                        status_code=response.status, detail="CF API is down"
+                    )
+                data = await response.json()
+                return data["result"][0]["rating"]
+        except Exception as e:
+            return {"Error": "Please try again later" + str(e)}
 
 
 @app.get("/get_unsolved_questions/{handle}/{lowerlimit}/{upperlimit}")
@@ -99,5 +69,57 @@ async def get_unsolved_questions(handle: str, lowerlimit: str, upperlimit: str):
                         if question["name"] not in solved_questions:
                             unsolved_questions.append(question["name"])
                     return unsolved_questions
+        except Exception as e:
+            return {"Error": "Please try again later" + str(e)}
+
+
+@app.get("/get_questions/{handle}")
+async def get_questions(handle: str):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(
+                "https://codeforces.com/api/user.info?handles=" + handle
+            ) as response:
+                if response.status != 200:
+                    return HTTPException(
+                        status_code=response.status, detail="CF API is down"
+                    )
+                data = await response.json()
+                rating = data["result"][0]["rating"]
+                lowerlimit = str((rating // 100) * 100 - 200)
+                upperlimit = str((rating // 100) * 100 + 200)
+                print(lowerlimit, upperlimit)
+                async with session.get(
+                    "https://acodedaily.com/api/v2/ladder?startRating="
+                    + lowerlimit
+                    + "&endRating="
+                    + upperlimit
+                ) as response:
+                    if response.status != 200:
+                        return HTTPException(
+                            status_code=response.status, detail="ACD API is down"
+                        )
+                    questions_list = await response.json()
+                    async with session.get(
+                        "https://codeforces.com/api/user.status?handle=" + handle
+                        # + "&from=1&count=10000"
+                    ) as response:
+                        if response.status != 200:
+                            return HTTPException(
+                                status_code=response.status, detail="CF API is down"
+                            )
+                        data = await response.json()
+                        solved_questions = list(
+                            set(
+                                item["problem"]["name"]
+                                for item in data["result"]
+                                if item["verdict"] == "OK"
+                            )
+                        )
+                        unsolved_questions = []
+                        for question in questions_list["data"]:
+                            if question["name"] not in solved_questions:
+                                unsolved_questions.append(question["name"])
+                        return unsolved_questions
         except Exception as e:
             return {"Error": "Please try again later" + str(e)}
